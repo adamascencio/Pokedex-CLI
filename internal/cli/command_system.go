6f9bf1ffc6230c50dd8990a7b09a071beb129bd5
adamascencio <ascencioadam@gmail.com>
2026-02-16 12:16:45 -0800

    update cli to read input using readline instead of bufio

--- internal/cli/command_system.go
+++ internal/cli/command_system.go
@@ -1,12 +1,15 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/adamascencio/pokedexcli/internal/pokecache"
 )
+
+var ErrExitRequested = errors.New("exit requested")
 
 func CleanInput(text string) []string {
 	words := strings.Fields(text)
@@ -18,6 +21,19 @@ func CleanInput(text string) []string {
 
 func CommandExit(cfg *AppState, cache *pokecache.Cache, area string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
+	return ErrExitRequested
+}
+
+func CreateHistoryFile() string {
+	f, err := os.OpenFile("/tmp/readline.tmp", os.O_CREATE|os.O_EXCL, 0644)
+	if err != nil {
+		panic("Unable to open tmp file")
+	}
+	f.Close()
+	return "/tmp/readline.tmp"
+}
+
+func DeleteHistoryFile() error {
+	err := os.Remove("/tmp/readline.tmp")
+	return err
 }
--- main.go
+++ main.go
@@ -1,41 +1,88 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
-	"os"
+	"io"
+	"log"
 	"time"
 
 	"github.com/adamascencio/pokedexcli/internal/api"
 	"github.com/adamascencio/pokedexcli/internal/cli"
 	"github.com/adamascencio/pokedexcli/internal/pokecache"
+	"github.com/chzyer/readline"
 )
 
+func filterInput(r rune) (rune, bool) {
+	switch r {
+	// block CtrlZ feature
+	case readline.CharCtrlZ:
+		return r, false
+	}
+	return r, true
+}
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	urls := cli.AppState{
+	historyFile := cli.CreateHistoryFile()
+	defer cli.DeleteHistoryFile()
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:              "\033[31m»\033[0m ",
+		HistoryFile:         historyFile,
+		InterruptPrompt:     "^C",
+		EOFPrompt:           "exit",
+		HistorySearchFold:   true,
+		FuncFilterInputRune: filterInput,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	l.CaptureExitSignal()
+	state := cli.AppState{
 		Next:     api.LocationsURL,
 		Captures: make(map[string]api.Pokemon),
 	}
 	cache := pokecache.NewCache(10 * time.Second)
+	log.SetOutput(l.Stderr())
 	for {
-		fmt.Print("Pokedex > ")
-		for scanner.Scan() {
-			text := cli.CleanInput(scanner.Text())
-			if len(text) == 0 {
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				if err := cli.DeleteHistoryFile(); err != nil {
+					fmt.Println(err)
+				}
+				break
+			} else {
 				continue
 			}
-			cmd := text[0]
-			var arg string
-			if len(text) > 1 {
-				arg = text[1]
+		} else if err == io.EOF {
+			if err := cli.DeleteHistoryFile(); err != nil {
+				fmt.Println(err)
 			}
-			if cliFunc, ok := cli.Commands[cmd]; ok {
-				cliFunc.Callback(&urls, cache, arg)
-			} else {
-				fmt.Println("Unknown command")
+			break
+		}
+
+		args := cli.CleanInput(line)
+
+		if len(args) == 0 {
+			continue
+		}
+		cmd := args[0]
+		arg := ""
+		if len(args) > 1 {
+			arg = args[1]
+		}
+
+		if cliFunc, ok := cli.Commands[cmd]; ok {
+			if err := cliFunc.Callback(&state, cache, arg); err != nil {
+				if errors.Is(err, cli.ErrExitRequested) {
+					cli.DeleteHistoryFile()
+					break
+				}
+				fmt.Println(err)
 			}
-			fmt.Print("Pokedex > ")
+		} else {
+			fmt.Println("Unknown command")
 		}
 	}
 }
